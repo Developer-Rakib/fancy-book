a510a2381326260b1a03245072eac35345049a13
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-05 15:02:06 +0600

    complete

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -12,15 +12,12 @@ const Blogs = () => {
                 <AccordionItem className='my-2'>
                     <AccordionItemHeading className='border inline-block rounded px-5'>
                         <AccordionItemButton>
-                            <h4 className='text-lg sm:text-2xl'>What harsh truths do you prefer to ignore?</h4>
+                            <h4 className='text-lg sm:text-2xl'>What is Context API?</h4>
                         </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                         <p className='inline-block rounded py-'>
-                            Exercitation in fugiat est ut ad ea cupidatat ut in
-                            cupidatat occaecat ut occaecat consequat est minim minim
-                            esse tempor laborum consequat esse adipisicing eu
-                            reprehenderit enim.
+                            The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.The Context API can be used to share data with multiple components, without having to pass data through props manually. For example, some use cases the Context API is ideal for: theming, user language, authentication, etc.
                         </p>
                     </AccordionItemPanel>
                 </AccordionItem>
@@ -28,15 +25,12 @@ const Blogs = () => {
                 <AccordionItem className='my-2'>
                     <AccordionItemHeading className='border inline-block rounded px-5'>
                         <AccordionItemButton>
-                            <h4 className='text-lg sm:text-2xl'>What harsh truths do you prefer to ignore?</h4>
+                            <h4 className='text-lg sm:text-2xl'>What Is Semantic HTML Tag?</h4>
                         </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                         <p className='inline-block rounded py-'>
-                            Exercitation in fugiat est ut ad ea cupidatat ut in
-                            cupidatat occaecat ut occaecat consequat est minim minim
-                            esse tempor laborum consequat esse adipisicing eu
-                            reprehenderit enim.
+                            HTML was originally created as a markup language to describe documents on the early internet. As the internet grew and was adopted by more people, its needs changed.Where the internet was originally intended for sharing scientific documents, now people wanted to share other things as well. Very quickly, people started wanting to make the web look nicer.Because the web was not initially built to be designed, programmers used different hacks to get things laid out in different ways. Rather than using the table tag describe information using a table, programmers would use them to position other elements on a page.As the use of visually designed layouts progressed, programmers started to use a generic “non-semantic” tag like div.They would often give these elements a class or id attribute to describe their purpose.As HTML5 is still relatively new, this use of non-semantic elements is still very common on websites today.
                         </p>
                     </AccordionItemPanel>
                 </AccordionItem>
@@ -44,15 +38,12 @@ const Blogs = () => {
                 <AccordionItem className='my-2'>
                     <AccordionItemHeading className='border inline-block rounded px-5'>
                         <AccordionItemButton>
-                            <h4 className='text-lg sm:text-2xl'>What harsh truths do you prefer to ignore?</h4>
+                            <h4 className='text-lg sm:text-2xl'>What is The Difference Between Inline & Inline Block Elements?</h4>
                         </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                         <p className='inline-block rounded py-'>
-                            Exercitation in fugiat est ut ad ea cupidatat ut in
-                            cupidatat occaecat ut occaecat consequat est minim minim
-                            esse tempor laborum consequat esse adipisicing eu
-                            reprehenderit enim.
+                            Inline The element doesn't start on a new line and only occupy just the width it requires. You can't set the width or height. inline-block It's formatted just like the inline element, where it doesn't start on a new line. BUT, you can set width and height values.
                         </p>
                     </AccordionItemPanel>
                 </AccordionItem>
